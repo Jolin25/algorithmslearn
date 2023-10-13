446afe998b07d088da0adc3a0e8254a0346395d2
Joly163 <18990341730@163.com>
2023-10-13 20:54:20 +0800

    .

---
+++ src/top/algorithms/learn/structure/tree/test/Tree0.java
@@ -0,0 +1,13 @@
+package top.algorithms.learn.structure.tree.test;
+
+import java.util.HashMap;
+
+/**
+ * @author jrl
+ * @date Create in 13:26 2023/7/11
+ */
+public class Tree0 {
+	public static void main(String[] args) {
+		HashMap<Object, Object> map = new HashMap<>(16);
+	}
+}
